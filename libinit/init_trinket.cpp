c5309fc579a2a44e8a2ed9720ebca9aca3cc2e4b
qaotyk <mayniakgd@gmail.com>
2026-05-27 19:39:58 -0600

    sm6125-common: Adapt libinit to moto devices
    * Use correct path in declarations
    
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- libinit/init_trinket.cpp
+++ libinit/init_trinket.cpp
@@ -37,7 +37,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <init/DeviceLibinit.h>
+#include <libinit/DeviceLibinit.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
