e4d162ade9ef2fb0dedaffc9415921bfc4f6bcfd
qaotyk <mayniakgd@gmail.com>
2026-05-30 19:11:49 -0600

    sm6125-common: Build graphics, display, and Vulkan
    * Set permissions for graphics > frameworks
    * Set graphics properties
    
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- common.mk
+++ common.mk
@@ -94,7 +94,22 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-qti \
     android.hardware.camera.provider@2.4-service
 
-# Display
+# Display - Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.graphics.composer@2.4-service \
+
+# Display - Vendor Specifics
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.allocator-service \
+
+# Display - Hardware
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.trinket \
+    memtrack.trinket
 
 # DRM
 
@@ -185,11 +200,25 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.location.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.xml
 
+# Permissions - Graphics
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
 # Properties - GPS/GNSS
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.overlay.izat.optin=rro \
     persist.vendor.gps.handover=true \
     ro.hardware.gps=default
+
+# Properties - Graphics
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno \
+    ro.opengles.version=196610
 
 # Ramdisk
 
