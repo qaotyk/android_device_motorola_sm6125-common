194e1b3288eb61008d8ce2cb53e34cf6bab4204c
qaotyk <mayniakgd@gmail.com>
2026-05-31 16:14:22 -0600

    sm6125-common: Add RIL and telephony HALs
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- common.mk
+++ common.mk
@@ -276,6 +276,22 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries-ext-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
 # RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.deprecation@1.0 \
+    vendor.qti.hardware.radio.am@1.0 \
+    vendor.qti.hardware.radio.lpa@1.0 \
+    vendor.qti.hardware.radio.qcrilhook@1.0 \
+    vendor.qti.hardware.radio.qtiradio@1.0 \
+    vendor.qti.hardware.radio.qtiradio@2.4 \
+    vendor.qti.hardware.radio.uim@1.2 \
+    vendor.qti.hardware.radio.uim_remote_client@1.0 \
+    vendor.qti.hardware.radio.uim_remote_server@1.0 \
+    vendor.qti.hardware.data.connection@1.1 \
+    vendor.qti.hardware.data.latency@1.0 \
+    vendor.qti.data.factory@2.2 \
+    vendor.qti.hardware.embmssl@1.1
 
 # Sensors
 
