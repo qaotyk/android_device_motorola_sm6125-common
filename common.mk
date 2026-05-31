68b05f88162b2cfc2ba20ab99ffc582225309515
qaotyk <mayniakgd@gmail.com>
2026-05-30 20:51:30 -0600

    sm6125-common: Import and configure Power HAL
    Configure open-source AIDL Power HAL and import powerhint.xml configuration from the dump.
    
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- common.mk
+++ common.mk
@@ -196,6 +196,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2-service
 
 # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Overlay
 
