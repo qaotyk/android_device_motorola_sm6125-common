bd2d8382901c34bd46f1df75e2889f557195d46a
qaotyk <mayniakgd@gmail.com>
2026-05-30 18:46:23 -0600

    sm6125-common: Build audio HAL, bluetooth, and soundtrigger packages
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- common.mk
+++ common.mk
@@ -48,9 +48,29 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Audio
+# Audio - Amplifier
+PRODUCT_PACKAGES += \
+    audio_amplifier.trinket \
+    audio.primary.trinket \
+    sound_trigger.primary.trinket
 
-# Audio - configs
+# Audio - HAL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service
+
+# Audio - Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-service \
+    android.hardware.bluetooth@1.0-service
+
+# Audio - Soundtrigger
+PRODUCT_PACKAGES += \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service
+
+# Audio - Configs
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(COMMON_PATH)/audio/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
