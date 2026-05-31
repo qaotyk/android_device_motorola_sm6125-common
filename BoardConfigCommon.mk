09137ec7ff3712a611f1c7ab5a93276c62985dfc
qaotyk <mayniakgd@gmail.com>
2026-05-30 21:28:51 -0600

    sm6125-common: Import QVA VINTF manifest
    Import manifest-qva.xml to define ANT+ HAL support and declare it in BoardConfigCommon.mk.
    
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,7 +179,9 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # VINTF
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(COMMON_PATH)/vintf/manifest.xml \
+    $(COMMON_PATH)/vintf/manifest-qva.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml
 
 # Inherit from the proprietary version
