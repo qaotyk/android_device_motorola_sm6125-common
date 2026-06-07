e941259a4bea3b187db2d39b77f9d8cbea1d0c6c
qaotyk <mayniakgd@gmail.com>
2026-06-06 18:11:47 -0600

    sm6125-common: Set USB service and permissions
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- common.mk
+++ common.mk
@@ -288,6 +288,13 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
 # USB configs
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # RenderScript
 PRODUCT_PACKAGES += \
